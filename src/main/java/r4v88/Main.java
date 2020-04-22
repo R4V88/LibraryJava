8b5c590f572f305f158e8715e6f9c3b32edbeaef
R4v88 <sochacki.rfl@gmail.com>
2020-04-22 19:40:50 +0200

    Main fixed - it should be deleted instead

--- src/main/java/r4v88/Main.java
+++ src/main/java/r4v88/Main.java
@@ -16,26 +16,26 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws UserWithIdDoesNotExist, UserWithNameAndLastNameDoesNotExist, UserWithEmailDoesNotExist, UserWithLoginEmailAlreadyExist, DateOfBirthIsNotValid, LoginIsNotValid, PasswordIsNotValid, EmailIsNotValid, UserWithLoginDoesNotExist {
-        User wacek = User.uilder()
-                .setName("tak")
-                .setLastname("nie")
-                .setLogin("wacek2")
-                .setPassword("1Ab*45678")
-                .setDateOfBirth("11.08.2018")
-                .setEmail("email@gmail.mail")
-                .setGender(Gender.MALE)
-                .setRole(Role.USER)
+        User wacek = User.builder()
+                .name("tak")
+                .lastname("nie")
+                .login("wacek2")
+                .password("1Ab*45678")
+                .dateOfBirth("11.08.2018")
+                .email("email@gmail.mail")
+                .gender(Gender.MALE)
+                .role(Role.USER)
                 .build();
 
-        User marek = new User.Builder()
-                .setName("marek")
-                .setLastname("jest")
-                .setLogin("marekk")
-                .setPassword("1Ab*45678")
-                .setDateOfBirth("11.10.1994")
-                .setEmail("email@dzimejl.mail")
-                .setGender(Gender.FEMALE)
-                .setRole(Role.ADMIN)
+        User marek = User.builder()
+                .name("marek")
+                .lastname("jest")
+                .login("marekk")
+                .password("1Ab*45678")
+                .dateOfBirth("11.10.1994")
+                .email("email@dzimejl.mail")
+                .gender(Gender.FEMALE)
+                .role(Role.ADMIN)
                 .build();
 
 
@@ -50,36 +50,19 @@ public class Main {
 
 //        System.out.println(book.getId());
 
-        Author author = new Author.Builder()
-                .setName("Name")
-                .setLastname("LastName")
-                .setDateOfBirth("19.12.2019")
-                .build();
-
-//        UserDao userDao = UserDaoImpl.getInstance();
-//        userDao.saveUser(user);
-//
-//        userDao.updateUser(user, 1);
+        Author author = new Author("Name", "LastName", "19.12.2019");
 
         UserService userService = UserServiceImpl.getInstance();
-//        System.out.println(userService.getUserById(1).toString());
-//        System.out.println(userService.getUserByNameAndLastname("tak", "nie"));
-//        userService.updateUserName("Karol", 1);
-//        System.out.println(userService.getUserById(1).toString());
-//        userService.createUser(marek);
-//        System.out.println(userService.getUserByNameAndLastname("marek", "jest"));
-//        System.out.println(userService.getUserById(1));
-//        System.out.println(userService.getUserByNameAndLastname("Tom", "Tailor"));
 
-        User karolek = new User.Builder()
-                .setName("Karolak")
-                .setLastname("Worek")
-                .setLogin("loginbb")
-                .setEmail("taki@mejl")
-                .setPassword("1Ab*45678")
-                .setDateOfBirth("11.12.2018")
-                .setGender(Gender.MALE)
-                .setRole(Role.USER)
+        User karolek = User.builder()
+                .name("Karolak")
+                .lastname("Worek")
+                .login("loginbb")
+                .email("taki@mejl")
+                .password("1Ab*45678")
+                .dateOfBirth("11.12.2018")
+                .gender(Gender.MALE)
+                .role(Role.USER)
                 .build();
 //        userService.createUser(karolek);
 ////        userService.createUser(marek);
